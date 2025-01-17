fb54e1cbd2b8adc1bb6505f6571a727caf55b17a
Sulaimon Nurudeen Adisa <snurudeen21@gmail.com>
2025-01-17 03:46:24 +0200

    main

---
+++ main.c
@@ -0,0 +1,30 @@
+#include "shell.h"
+
+/**
+* main - execute line, tokens and excecute functions
+*
+* Return: 0
+*/
+
+int main(void)
+{
+	bool is_pipe = false;
+	ssize_t bytes;
+
+	while (true && !is_pipe)
+	{
+		if (isatty(STDIN_FILENO) == 0)
+		{
+			is_pipe = true;
+		}
+		else
+			my_prompt();
+		bytes = read_line();
+		if (bytes == EOF)
+		{
+			is_pipe = true;
+			exit(0);
+		}
+	}
+	return (0);
+}
