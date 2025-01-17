a47d43683c574b71a6eb4df58f204c7d023285b4
Sulaimon Nurudeen Adisa <snurudeen21@gmail.com>
2025-01-17 03:28:19 +0200

    header file

---
+++ shell.h
@@ -0,0 +1,29 @@
+#ifndef SHELL_H
+#define SHELL_H
+
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/wait.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <sys/stat.h>
+
+extern char **environ;
+
+void _exec(char **args);
+void free_buf(char **buf);
+ssize_t read_line(void);
+void my_prompt(void);
+char **split_line(char *line);
+int _strlen(char *s);
+char *_strcpy(char *dest, const char *src);
+char *_strcat(char *dest, char *src);
+char *_strdup(char *s);
+int _strcmp(char *s1, char *s2);
+char *_pathfinder(char *command);
+char *path_finder(char *command);
+
+
+#endif /* SHELL_H */
