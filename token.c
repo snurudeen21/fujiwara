97e3ddd493bbe33b582f21d5d546fa99a3f96561
Sulaimon Nurudeen Adisa <snurudeen21@gmail.com>
2025-01-17 03:40:52 +0200

    token_ line into tokens

---
+++ token.c
@@ -0,0 +1,29 @@
+#include "shell.h"
+
+/**
+* split_line - Tokinize command prompt in line into an array
+* @line: pointer array to tokenize
+*
+* Return: containing spilt command prompt
+*/
+
+char **split_line(char *line)
+{
+	int length = 0;
+	char **tokens = malloc(sizeof(char *) * _strlen(line));
+
+	char *delimiters = " \n\t\r";
+	char *token = strtok(line, delimiters);
+
+	while (token != NULL)
+	{
+		tokens[length] = _strdup(token);
+		length++;
+
+		token = strtok(NULL, delimiters);
+	}
+
+	tokens[length] = NULL;
+	free(token);
+	return (tokens);
+}
