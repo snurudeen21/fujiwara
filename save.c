491f41ef60568b605751560c8f0710d878d5bdf8
Sulaimon Nurudeen Adisa <snurudeen21@gmail.com>
2025-01-18 16:38:10 +0200

    Update save.c

--- save.c
+++ save.c
@@ -9,10 +9,34 @@ void save_to_csv(const char *filename)
 
     // Write header row
     fprintf(file, "Cx,Cy,Cn,Ck\n");
+    
+    double Cx[size];
+	double Cy[size];
+	double Cn[size];
+	double Ck[size];
 
+    for (int i = 0; i < size;  i++)
+				{
+					Cx[i] = result[0] + result[1]*cos((*(a+i))*M_PI/180) + result[2]*(cos(3*(*(a+i))*M_PI/180)) + result[3]*(cos(5*(*(a+i))*M_PI/180));
+				}
+	for (int j = 0; j < size;  j++)
+				{
+					Cy[j] = result[4]*sin((*(a+j))*M_PI/180) + result[5]*(sin(3*(*(a+j))*M_PI/180)) + result[6]*(sin(5*(*(a+j))*M_PI/180));
+				}
+	for (int k = 0; k < size;  k++)
+				{
+					Cn[k] = result[7]*sin((*(a+k))*M_PI/180) + result[8]*(sin(2*(*(a+k))*M_PI/180)) + result[9]*(sin(3*(*(a+k))*M_PI/180));
+				}
+		
+	for (int m = 0; m < size;  m++)
+				{
+					Ck[m] = result[10]*sin((*(a+m))*M_PI/180) + result[11]*(sin(2*(*(a+m))*M_PI/180)) + result[12]*(sin(3*(*(a+m))*M_PI/180)) + result[13]*(sin(5*(*(a+m))*M_PI/180));	
+				}
+		
+    
     // Write data rows
-    for (int i = 0; i < size; i++) {
-        fprintf(file, "%f,%f,%f,%f\n", Cx[i], Cy[i], Cn[i], Ck[i]);
+    for (int c = 0; c < size; c++) {
+        fprintf(file, "%f,%f,%f,%f\n", Cx[c], Cy[c], Cn[c], Ck[c]);
     }
 
     fclose(file);
