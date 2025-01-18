7a2da5b22e23f221cec5c0da1c6b945a8fed1d96
Sulaimon Nurudeen Adisa <snurudeen21@gmail.com>
2025-01-18 02:52:32 +0200

    Update line.c

--- line.c
+++ line.c
@@ -40,13 +40,6 @@ ssize_t read_line(void)
 		}
 
 		args = split_line(line);
-		int args_count = 0;
-
-		// Loop through each element in the args array until NULL is encountered
-		while (args[args_count] != NULL)
-		{
-			args_count++; // Increment the counter
-		}
 		
 		free(line);
 		if (args[0] == NULL)
@@ -57,15 +50,22 @@ ssize_t read_line(void)
 		
 		if (strcmp(args[0], "angles") == 0)
         	{
-            		int *angle_of _attack =  angles(args);
+			int args_count = 0;
+
+			// Loop through each element in the args array until NULL is encountered
+			while (args[args_count] != NULL)
+			{
+				args_count++; // Increment the counter
+			}
+            		int *a =  angles(args);
         	}
 
 		if (strcmp(args[0], "analyze") == 0)
         	{
-			
+			double Cx = 
             		double result = analyze(args);
 			for (i = 0; i < (args_count-1);  i++)
-				Cx[i] result[0] + result[1]*round(cos(radians(a)), 6) + result[2]*round(.cos(radians(3*a)), 6) + result[3]*round(cos(radians(5*a)), 6)	
+				Cx[i] = result[0] + result[1]*cos(a*M_PI/180), 6) + result[2]*round(cos(radians(3*a)), 6) + result[3]*round(cos(radians(5*a)), 6)	
         	}
 		
    		buflen = 0;
