2d4acb0e625c786610f8a37b1a7d24a185b8e1ca
Sulaimon Nurudeen Adisa <snurudeen21@gmail.com>
2025-01-18 13:00:42 +0200

    Create save.c

---
+++ save.c
@@ -0,0 +1,18 @@
+void save_to_csv(const char *filename) {
+    FILE *file = fopen(filename, "w");
+    if (file == NULL) {
+        perror("Error opening file");
+        return;
+    }
+
+    // Write header row
+    fprintf(file, "Cx,Cy,Cn,Ck\n");
+
+    // Write data rows
+    for (int i = 0; i < size; i++) {
+        fprintf(file, "%f,%f,%f,%f\n", Cx[i], Cy[i], Cn[i], Ck[i]);
+    }
+
+    fclose(file);
+    printf("Data successfully saved to %s\n", filename);
+}
